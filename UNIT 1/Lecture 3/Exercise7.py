e721bd4dc40001a11821f81e7633d40d8f2b67c7
Lundli <46197518+Lundli@users.noreply.github.com>
2019-07-04 14:16:30 +0200

    Create Exercise7.py

---
+++ UNIT 1/Lecture 3/Exercise7.py
@@ -0,0 +1,26 @@
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight):
+        # Your code here
+        pass
+    def getWeight(self):
+        # Your code here
+        pass
+    def __str__(self):
+        # Your code here
+        pass
+Code Editor
+
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight):
+        # Your code here
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getWeight(self):
+        # Your code here
+        return self.weight
+    def __str__(self):
+        # Your code here
+        return str(self.src) + "->" + str(self.dest) + " (" + str(self.weight) + ")"
+
